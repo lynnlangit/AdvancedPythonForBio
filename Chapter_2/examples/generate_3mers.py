f200d8c4ddd6fff5f57c37bb073aa6fdb9d16292
Lynn Langit <lynnlangit@live.com>
2017-02-28 17:52:57 -0800

    return pattern for VSCode

--- Chapter_2/examples/generate_3mers.py
+++ Chapter_2/examples/generate_3mers.py
@@ -1,8 +1,13 @@
-def generate_trimers(): 
-    bases = ['A', 'T', 'G', 'C'] 
-    result = [] 
-    for base1 in bases: 
-        for base2 in bases: 
-            for base3 in bases: 
-                result.append(base1 + base2 + base3) 
-    return result 
+def generate_trimers():
+    bases = ['A', 'T', 'G', 'C']
+    result = []
+    for base1 in bases:
+        for base2 in bases:
+            for base3 in bases:
+                result.append(base1 + base2 + base3)
+    for i in result:
+        answer = i + " "
+        print 'Some', answer
+
+generate_trimers()
+
---
+++ Chapter_2/examples/hello.py
@@ -0,0 +1,7 @@
+def square(x):
+    y = x * x
+    return y
+
+toSquare = 10
+result = square(toSquare)
+print "The result of " + str(toSquare) + " squared is " + str(result)
