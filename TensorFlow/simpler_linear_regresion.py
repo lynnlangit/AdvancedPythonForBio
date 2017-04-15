6b1d7f7280da1efdb9237ac20211834ccf044c4a
Lynn Langit <lynnlangit@live.com>
2017-04-15 15:57:31 -0700

    update comments for TensorFlow sample

--- TensorFlow/simpler_linear_regresion.py
+++ TensorFlow/simpler_linear_regresion.py
@@ -4,12 +4,16 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 features = [tf.contrib.layers.real_valued_column("x", dimension=1)]
-# Types are linear regression,logistic regression, linear classification, logistic classification, many neural network classifiers and regressors 
-estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)  # Invoke training (fitting) & evaluation(inference) via the Estimator   
+# Types are linear regression,logistic regression, linear classification, 
+# logistic classification, many neural network classifiers and regressors 
+# Invoke training (fitting) & evaluation(inference) via the Estimator 
+estimator = tf.contrib.learn.LinearRegressor(feature_columns=features)
 
 x = np.array([1., 2., 3., 4.])
 y = np.array([0., -1., -2., -3.])
 input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x}, y, batch_size=4,num_epochs=1000)
 
-estimator.fit(input_fn=input_fn, steps=1000)            # Run 1000 training steps use the `fit` method & pass in the training data set
-estimator.evaluate(input_fn=input_fn)                   # Evaluate - In a real example, use a separate validation and testing data set to avoid overfitting
+# Use the `fit` method & pass in the training data set run 1000 training steps
+# Evaluate - in a real example, use a separate validation & testing data set to avoid overfitting  
+estimator.fit(input_fn=input_fn, steps=1000)
+estimator.evaluate(input_fn=input_fn)
