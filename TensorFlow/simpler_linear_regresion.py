789e438506edb50e754bba3bce012ec03989e097
Lynn Langit <lynnlangit@live.com>
2017-05-09 19:06:26 -0700

    surpressed tf warnings

--- TensorFlow/simpler_linear_regresion.py
+++ TensorFlow/simpler_linear_regresion.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
+tf.logging.set_verbosity(tf.logging.ERROR)
+
 features = [tf.contrib.layers.real_valued_column("x", dimension=1)]
 # Types are linear/logistic regression, linear/logistic classification
 # And many neural network regressors and classifiers 
@@ -16,11 +18,6 @@ input_fn = tf.contrib.learn.io.numpy_input_fn({"x":x}, y, batch_size=4,num_epoch
 # Use the `fit` method & pass in the training data set run 1000 training steps
 # Evaluate - in a real example, use a separate validation & testing data set to avoid overfitting  
 estimator.fit(input_fn=input_fn, steps=1000)
-estimator.evaluate(input_fn=input_fn)
+print(estimator.evaluate(input_fn=input_fn))
 
-# NOT WORKING, error as below...
-# Please switch to tf.summary.scalar. 
-# Note that tf.summary.scalar uses the node name instead of the tag. 
-# This means that TensorFlow will automatically de-duplicate summary names based on the scope they are created in. 
-# Also, passing a tensor or list of tags to a scalar summary op is no longer supported.
-# WARNING:tensorflow:Skipping summary for global_step, must be a float or np.float32.
+
