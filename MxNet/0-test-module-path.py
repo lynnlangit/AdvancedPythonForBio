a9ce0ffc828d17fd0370654eea99a7c00f9554e2
Samantha Langit <samanthalangit@gmail.com>
2017-07-25 15:01:45 -0700

    comment out path

--- MxNet/0-test-module-path.py
+++ MxNet/0-test-module-path.py
@@ -1,4 +1,4 @@
 import sys
 # adjust this path to include mxnet if needed
-sys.path.insert(0, "/Users/lynnlangit/mxnet/python")
+# sys.path.insert(0, "/Users/lynnlangit/mxnet/python")
 print(sys.path)
