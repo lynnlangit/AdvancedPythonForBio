eb7782968a4a69e3c2223838123aece825ef7632
Lynn Langit <lynnlangit@live.com>
2018-07-08 13:16:37 -0700

    added comments to map example

--- Chapter_5_Functional/examples/map.py
+++ Chapter_5_Functional/examples/map.py
@@ -6,22 +6,24 @@ dna_list = ['TAGC', 'ACGTATGC', 'ATG', 'ACGGCTAG']
 lengths = [] 
 for dna in dna_list: 
     lengths.append(len(dna)) 
-print(lengths)
+print("Shows lengths - uses 'for': " +str(lengths))
 at_contents = [] 
 for dna in dna_list: 
     at_contents.append((dna.count('A') + dna.count('T')) / len(dna)) 
-print(at_contents)
+print("Shows contents - uses 'for': " + str(at_contents))
 
 # using map
 def get_at(dna): 
     return (dna.count('A') + dna.count('T')) / len(dna) 
 
 lengths = map(len, dna_list) 
-print(lengths)
+print("Shows lengths - uses 'map': " +str(lengths))
+
 at_contents = map(get_at, dna_list) 
-print(at_contents)
+print("Shows contents - uses 'map': " + str(at_contents))
+
 at_contents = map( 
     lambda dna : (dna.count('A') + dna.count('T')) / len(dna), 
     dna_list 
 )
-print(at_contents)
+print("Shows contents - uses 'map': "+ str(at_contents))
